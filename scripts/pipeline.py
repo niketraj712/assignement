b8e4f36547076cc213f5a196dcc491d5b52dab35
niketraj712 <niketraj712@gmail.com>
2026-01-29 14:13:13 +0530

    Update pipeline.py

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import psycopg2
 
+df = pd.read_csv("sample_data/mock_messages.csv")
+
+conn = psycopg2.connect(
+    dbname="chatbot_analytics",
+    user="chatbot",
+    password="chatbot",
+    host="localhost"
+)
+
+df.to_sql("fact_message", conn, if_exists="append", index=False)
