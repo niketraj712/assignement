c3c7f71f6532d7323bbdd5788984e55e89e23a1b
niketraj712 <niketraj712@gmail.com>
2026-01-29 13:49:30 +0530

    Update task3_q2.sql

--- analysis/task3_q2.sql
+++ analysis/task3_q2.sql
@@ -1,1 +1,17 @@
 
+SELECT
+    session_id,
+    ip_address,
+    session_starttstamp,
+    LAG(session_endtstamp) OVER (
+        PARTITION BY ip_address
+        ORDER BY session_starttstamp
+    ) AS prev_session_endtstamp,
+    EXTRACT(EPOCH FROM (
+        session_starttstamp -
+        LAG(session_endtstamp) OVER (
+            PARTITION BY ip_address
+            ORDER BY session_starttstamp
+        )
+    )) AS idle_time_seconds
+FROM fact_session;
