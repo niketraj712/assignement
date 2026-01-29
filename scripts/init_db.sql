67de2a7a3a7df3a8e805717c496e3c5578ddbcfa
niketraj712 <niketraj712@gmail.com>
2026-01-29 13:47:09 +0530

    Update init_db.sql

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,1 +1,20 @@
+INSERT INTO dim_website VALUES
+(gen_random_uuid(), 'shop.sg', 'ecommerce'),
+(gen_random_uuid(), 'gov.sg', 'public');
 
+-- Users
+INSERT INTO dim_user VALUES
+(gen_random_uuid(), (SELECT website_id FROM dim_website LIMIT 1), 'en', 'SG', 'Chrome', now()),
+(gen_random_uuid(), (SELECT website_id FROM dim_website OFFSET 1 LIMIT 1), 'en', 'SG', 'Safari', now());
+
+-- Sessions
+INSERT INTO fact_session VALUES
+(gen_random_uuid(), (SELECT user_id FROM dim_user LIMIT 1),
+ (SELECT website_id FROM dim_website LIMIT 1),
+ '192.168.0.12', 'web', 'faq', 'completed',
+ '2026-01-22 08:30:00', '2026-01-22 08:45:00', now()),
+
+(gen_random_uuid(), (SELECT user_id FROM dim_user OFFSET 1 LIMIT 1),
+ (SELECT website_id FROM dim_website OFFSET 1 LIMIT 1),
+ '192.168.0.12', 'web', 'support', 'abandoned',
+ '2026-01-22 09:01:15', '2026-01-22 09:05:00', now());
